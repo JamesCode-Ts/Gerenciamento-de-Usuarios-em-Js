5555182332e959ce7286ed53bb30cd5d8135e03a
JamesCode-Ts@github.com <jameslob42@gmail.com>
2022-10-20 22:48:47 -0300

    Criando tr da tabela

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ var fiels = document.querySelectorAll("#form-user-create [name]");/** Seleciona 
 var user = {};
 
 
+function addLine(dataUser){
+
+var tr = document.createElement("tr"); /**cria o tr */
+
+
+tr.innerHTML = `
+
+<tr>
+<td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+<td>${dataUser.name}</td>
+<td>${dataUser.email}</td>
+<td>${dataUser.admin}</td>
+<td>${dataUser.birth}</td>
+<td>
+  <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+  <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+</td>
+</tr>
+`;
+
+document.getElementById("table-users").appendChild(tr); /** Acrescendo o tr criado na tabela com os dados passados. */
+
+
+
+}
+
+
 document.getElementById("form-user-create").addEventListener("submit", function(event){
 
 event.preventDefault(); /**Cancela o carregamento padrão da atualização após apertar o submit. */
 
-fiels.forEach(function(field, index){ 
+fiels.forEach(function(field, index){ /** O forEach serve para percorrer todos os campos de name. 
+                                          E oferece uma função como tratamento para cada campo name.*/
 
     if(field.name == "gender"){
 
@@ -26,7 +54,7 @@ fiels.forEach(function(field, index){
     }
 });
 
-console.log(user);
+addLine(user);
 
 });
 
