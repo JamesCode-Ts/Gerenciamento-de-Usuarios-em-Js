9a068f82e2ba8a8266ae491a9f34fc612a217aed
JamesCode-Ts@github.com <jameslob42@gmail.com>
2022-10-21 01:47:24 -0300

    MVC

---
+++ Models/User.js
@@ -0,0 +1,18 @@
+class User{
+
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+
+    }
+
+}
